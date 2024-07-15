48bb7ef6b70ed3ea20ed7a13466f2c9e4601898a
Ali <irakligoogoochani2005@gmail.com>
2024-07-15 16:12:30 +0330

    fixed lil mistakes

--- app.mjs
+++ app.mjs
@@ -1,17 +1,18 @@
 import { Telegraf } from "telegraf";
-import get_golde from "./request.mjs";
+import get_gold from "./request.mjs";
+// set bot token here
 var bot = new Telegraf("7031124598:AAEMNUWuC4Z1D_KP6CNMagM_dryEQarYwC4");
-
 let prev=0;
+// set a interval with 5secs time
 setInterval(()=>{
-
-    get_golde().then(
+    // call get_gold function
+    get_gold().then(
         function(value){
             if(value){
                 if(value[2]["v"][0]!=prev){
                     prev=value[2]["v"][0];
                     try{
-                    bot.telegram.sendMessage("@fast_gold_price",value[2]["v"][0]+"$");
+                    bot.telegram.sendMessage("@fast_gold_price",value[2]["v"][0]+" $");
                     }
                     catch(err){
                         console.log("not found the channel")
--- request.mjs
+++ request.mjs
@@ -1,18 +1,24 @@
-async function get_golde(){
+async function get_gold(){
+    // check is request ok
     try{
-    var ssid= await fetch("https://balancer11.netdania.com/StreamingServer/StreamingServer?xstream=1&v=3&dt=0&h=eyJnIjoibS5uZXRkYW5pYS5jb20iLCJhaSI6ImpzTVcgdjEuMiIsInByIjoiMyIsImF1IjoiaHR0cHM6Ly9tLm5ldGRhbmlhLmNvbS9jb21tb2RpdGllcyIsInF1cCI6MX0.&xcmd=W3sidCI6MSwiaSI6MSwibSI6MSwicyI6IlRSSkNSQlRSLm55IiwicCI6InNnX2RsYiIsImZsdCI6eyJtIjoxLCJmIjpbMSwyLDMsNiwxMCwxNywyNSwxMzhdfX0seyJ0IjoxLCJpIjoyLCJtIjoxLCJzIjoiWEFVVVNET1oiLCJwIjoiaWRjIiwiZmx0Ijp7Im0iOjEsImYiOlsxLDIsMyw2LDEwLDE3LDI1LDEzOF19fSx7InQiOjEsImkiOjMsIm0iOjEsInMiOiJYQUdVU0RPWiIsInAiOiJpZGMiLCJmbHQiOnsibSI6MSwiZiI6WzEsMiwzLDYsMTAsMTcsMjUsMTM4XX19LHsidCI6MSwiaSI6NCwibSI6MSwicyI6IkJUQ1VTRCIsInAiOiJjcnlwdG94Y2hhbmdlIiwiZmx0Ijp7Im0iOjEsImYiOlsxLDIsMyw2LDEwLDE3LDI1LDEzOF19fSx7InQiOjEsImkiOjUsIm0iOjEsInMiOiJVS09pbCIsInAiOiJmeGNtIiwiZmx0Ijp7Im0iOjEsImYiOlsxLDIsMyw2LDEwLDE3LDI1LDEzOF19fV0.&cb=?&ts=1715004542500");
+        // send a "get request" and fetch data from api
+    var content= await fetch("https://balancer11.netdania.com/StreamingServer/StreamingServer?xstream=1&v=3&dt=0&h=eyJnIjoibS5uZXRkYW5pYS5jb20iLCJhaSI6ImpzTVcgdjEuMiIsInByIjoiMyIsImF1IjoiaHR0cHM6Ly9tLm5ldGRhbmlhLmNvbS9jb21tb2RpdGllcyIsInF1cCI6MX0.&xcmd=W3sidCI6MSwiaSI6MSwibSI6MSwicyI6IlRSSkNSQlRSLm55IiwicCI6InNnX2RsYiIsImZsdCI6eyJtIjoxLCJmIjpbMSwyLDMsNiwxMCwxNywyNSwxMzhdfX0seyJ0IjoxLCJpIjoyLCJtIjoxLCJzIjoiWEFVVVNET1oiLCJwIjoiaWRjIiwiZmx0Ijp7Im0iOjEsImYiOlsxLDIsMyw2LDEwLDE3LDI1LDEzOF19fSx7InQiOjEsImkiOjMsIm0iOjEsInMiOiJYQUdVU0RPWiIsInAiOiJpZGMiLCJmbHQiOnsibSI6MSwiZiI6WzEsMiwzLDYsMTAsMTcsMjUsMTM4XX19LHsidCI6MSwiaSI6NCwibSI6MSwicyI6IkJUQ1VTRCIsInAiOiJjcnlwdG94Y2hhbmdlIiwiZmx0Ijp7Im0iOjEsImYiOlsxLDIsMyw2LDEwLDE3LDI1LDEzOF19fSx7InQiOjEsImkiOjUsIm0iOjEsInMiOiJVS09pbCIsInAiOiJmeGNtIiwiZmx0Ijp7Im0iOjEsImYiOlsxLDIsMyw2LDEwLDE3LDI1LDEzOF19fV0.&cb=?&ts=1715004542500");
     }
     catch(err){
+        // return a message for bad request
         console.log("bad request")
     }
-    if(ssid?.ok){
-    let res=await ssid.json();
+    // is body exist in response
+    if(content?.body){
+    // return json form from response
+    let res=await content.json();
     
     return res;
     }
-
+    // return nothing if body doesnt exist
     return "";
 }
 
-export default get_golde;
+
+export default get_gold;
 
